3059862676d258f6737aefd2e5af9c443bfe4d17
Mostafa ELMARAKSHI <morakshi88@gmail.com>
2022-02-04 22:20:59 +0100

    added shared resource copy construcotr

--- copyConstructor.cpp
+++ copyConstructor.cpp
@@ -7,52 +7,61 @@ using namespace std;
 class AYHAGA {
 private:
     int *num;
-
+    int *usersCount;    
 public:
     AYHAGA() {
 
     }
     AYHAGA(int x) {
         num = (int*)malloc(sizeof(int));
-       // num = new int(1);
         *num = x;
+        usersCount= (int*)malloc(sizeof(int));
+        *usersCount=1;
     }
     ~AYHAGA() {
-        if(num != nullptr) {
+        if(( num != nullptr)&&((*usersCount)==1)){
             free(num);
+            cout<<"Freeeeeee"<<endl;
         }
-        
-        //delete num;
+        (*usersCount)--;
     }
 
     //AYHAGA(const AYHAGA &src) = delete ; //no copy policy where a compilation will be trigger when copying and object to another
     //AYHAGA &operator=(const AYHAGA &src) = delete;
 
-    //exclusive ownership policy copy constructor
+    //Shared ownership policy
     AYHAGA(AYHAGA &src) {
         num = src.num;
-        src.num = nullptr;
+        usersCount=src.usersCount;
+        (*usersCount)++;
     }
-
+    //Shared ownership policy
     AYHAGA &operator=(AYHAGA &src) {
         //cout << this << std::endl << &src;
         if(this != &src){
             num = src.num;
-            src.num = nullptr;
+            usersCount=src.usersCount;
+            (*usersCount)++;
         }
 
         return *this;
     }
     void printInfo() {
         if(num != nullptr) {
-            std::cout << *num << std::endl;
+            std::cout << *num <<  "\tusers: " <<*usersCount  <<std::endl;
         }
-        std::cout << &num << "\t" << num << std::endl;
     }
+    void SetNum(int x)
+    {
+        *num=x;
+    }
+        //std::cout << &num << "\t" << num << std::endl;
+    
 };
 
 int main() {
 
+    AYHAGA z(30);
     AYHAGA x(10);
     //AYHAGA y(x);
     AYHAGA y;
@@ -60,8 +69,12 @@ int main() {
 
 
 
-    //x.printInfo();
+    x.printInfo();
     y.printInfo();
+    y.SetNum(20);
+    x.printInfo();
+    y.printInfo();
+    z.printInfo();
 
     return 0;
 }
